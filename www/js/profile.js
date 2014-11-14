835e0c9163a5097e89dd33a61d96de4c8156e921
Shinsaber <adrien.audemar@gmail.com>
2014-11-14 16:45:00 +0100

    1-1 Add feedback popup message on logout

--- www/js/profile.js
+++ www/js/profile.js
@@ -51,10 +51,6 @@ module.controller('LoginCtrl', function($scope, $state, $ionicPopup, LoginServic
     };
 });
 
-module.controller('LogoutCtrl', function($scope, LoginService) {
-    LoginService.logout();
-});
-
 module.controller('SignUpCtrl', function($scope, LoginService, SignUpService) {
 
     $scope.signupData = {};
@@ -244,16 +240,6 @@ module.config(function($stateProvider, $urlRouterProvider) {
                 'signupContent' :{
                     templateUrl: "templates/profile/signup.html",
                     controller: 'SignUpCtrl'
-                }
-            }
-        })
-
-        .state('app.profile.logout', {
-            url: "/logout",
-            views: {
-                'profileContent' :{
-                    templateUrl: "templates/profile/logout.html",
-                    controller: 'LogoutCtrl'
                 }
             }
         })
